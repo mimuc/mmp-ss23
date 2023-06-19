c4e2a7db9f2f1ad93e54ff93b709fd1635be3a5d
florianb <florian.bemmann@ifi.lmu.de>
2023-06-19 11:14:36 +0200

    tutorial 6

---
+++ 6 - unity/Codebeispiele/MMP Spaceship U6/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private bool enableKeyboardControl;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (enableKeyboardControl)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rb2d.AddForce(movement * speed);
+        }
+        else
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rb2d.MovePosition(mousePosition);
+        }
+    }
+}
+
+
