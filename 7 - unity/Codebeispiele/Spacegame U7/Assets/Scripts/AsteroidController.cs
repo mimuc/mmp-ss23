3595b0f6aaf6c849cdf70a1aa453d099bc03bf4e
florianb <florian.bemmann@ifi.lmu.de>
2023-06-26 15:59:01 +0200

    tutorial 7

---
+++ 7 - unity/Codebeispiele/Spacegame U7/Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.5f;
+    private Rigidbody2D rb2D;
+
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(0, -speed);
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (-transform.position.y > screenBounds.y * 2)
+        {
+           Destroy(this.gameObject);
+        }
+    }
+
+    
+}
---
+++ 7 - unity/Codebeispiele/Spacegame U7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rigidbody2D;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0f);
+        rigidbody2D.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Baaaaam");
+    }
+
+  
+}
