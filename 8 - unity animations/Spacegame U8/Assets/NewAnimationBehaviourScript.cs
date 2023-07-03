a4bc6074b572c994b443891633eddaa119793867
florianb <florian.bemmann@ifi.lmu.de>
2023-07-03 08:13:49 +0200

    tutorial 8

---
+++ 8 - unity animations/Spacegame U8/Assets/NewAnimationBehaviourScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewAnimationBehaviourScript : StateMachineBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rb2D = animator.GetComponent<Rigidbody2D>();
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rb2D.velocity = rb2D.velocity*0.99f;
+
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetTrigger("stop-movement-trigger");
+        }
+
+
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
